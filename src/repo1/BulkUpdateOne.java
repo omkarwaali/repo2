549c4beb6d234f9696f6aaedbafa49438577d09e
Omkar11 Mallikarjun <218187@NTTDATA.COM>
2026-01-01 19:38:37 +0530

    updated pom

---
+++ src/repo1/BulkUpdateOne.java
@@ -0,0 +1,14 @@
+package repo1;
+
+public class BulkUpdateOne {
+
+	public BulkUpdateOne() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("======================Executed");
+
+	}
+}
---
+++ src/repo1/BulkUpdateTwo.java
@@ -0,0 +1,14 @@
+package repo1;
+
+public class BulkUpdateTwo {
+
+	public BulkUpdateTwo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("======================Executed");
+
+	}
+}
